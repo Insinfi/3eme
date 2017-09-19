95c30f305c5f5458a1a2dfb92b7801f8d4f714f1
Insinfi <insinfigame@gmail.com>
2017-09-19 10:45:53 +0200

    02

--- Console01/Console01/Program.cs
+++ Console01/Console01/Program.cs
@@ -12,10 +12,15 @@ namespace Console01
             //Console.WriteLine("Hello world");
 
             test a = new test();                //Le "new" est obligatoire
-            int b;                              //Le "new" n'est pas nécessaire
+            System.Int32 b = 200;
+             b=100;                          //Le "new" n'est pas nécessaire
+            string resultat = b.ToString();
+            Console.WriteLine(resultat);
+
             int[] tab = new int[20];            //Le "new" est obligatoire
+
             test.DireBonjour();
-            Console.ReadKey();
+            Console.ReadKey();                  //Pause
         }
     }
 
