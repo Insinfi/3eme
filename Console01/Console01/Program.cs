6991b10deb9ecd8f38d428d75cfc6a4fb4f88024
Insinfi <insinfigame@gmail.com>
2017-09-19 10:04:28 +0200

    01

---
+++ Console01/Console01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world");
+
+            test a = new test();                //Le "new" est obligatoire
+            int b;                              //Le "new" n'est pas nécessaire
+            int[] tab = new int[20];            //Le "new" est obligatoire
+            test.DireBonjour();
+            Console.ReadKey();
+        }
+    }
+
+    class test
+    {
+        public static void DireBonjour()
+        {
+            Console.WriteLine("Bonjour");
+        }
+    }
+}
