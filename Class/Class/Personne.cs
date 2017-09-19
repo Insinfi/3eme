bb92793cb9e347505247b7d18fe1942b4939736b
Insinfi <insinfigame@gmail.com>
2017-09-19 12:25:56 +0200

    01

---
+++ Class/Class/Personne.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class
+{
+    class Personne
+    {
+        string _nom;
+        string _prenom;
+        
+        public string nom
+        {
+            get
+            {
+                return this._nom;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    this._nom = value;
+                else
+                {
+                    throw new ArgumentNullException("Chaine de caractère vide inerdite");
+                }
+                
+            }
+        }
+        public string prenom
+        {
+            get
+            {
+                return this._prenom;
+            }
+            set
+            {
+                if (value.Length != 0)
+                    this._prenom = value;
+                else
+                {
+                    throw new ArgumentNullException("Chaine de caractère vide inerdite");
+                }
+
+            }
+        }
+        public Personne():this("Empty","Empty")     //La définition par default fonctionne aussi
+        {
+            //this.nom = "Empty";
+            //this.prenom = "Empty";
+        }
+        public Personne(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+        public override string ToString()
+        {
+            return this._nom + " " + _prenom;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne("Dupont", "Bernard");
+            Console.WriteLine("Base:\t"+p1.nom);
+            p1.nom = "Dupond";
+            Console.WriteLine("Change:\t"+p1.nom);
+            try
+            {
+                p1.nom = "";
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Argument null");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("You have done shit!");
+            }
+
+            Console.WriteLine("Vide:\t"+p1.nom);
+            Console.ReadKey();
+        }
+    }
+}
