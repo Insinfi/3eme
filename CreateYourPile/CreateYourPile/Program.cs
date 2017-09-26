395bfc48dd24d1565eff536d6a05be67f292c5cd
Insinfi <insinfigame@gmail.com>
2017-09-26 09:10:46 +0200

    02

---
+++ CreateYourPile/CreateYourPile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateYourPile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pile<int> test = new Pile<int>;
+            Pile<string> test2 = new Pile<string>;
+            test.Add(10);
+            test.Add(20);
+
+            Console.Write(test.Get());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CreateYourPile/CreateYourPile/pile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateYourPile
+{
+    class Pile<T>
+    {
+        List<T> m_list;
+
+        public Pile()
+        {
+            m_list = new List<T>();
+        }
+
+        public void Add(T variable)
+        {
+            m_list.Add(variable);
+        }
+        public T Get()
+        {
+            if (this.m_list.Count > 0)
+            {
+                T element = this.m_list[this.m_list.Count - 1];
+                this.m_list.RemoveAt(this.m_list.Count - 1);
+                return element;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        public void Clear()
+        {
+            this.m_list.Clear();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.m_list.Count;
+            }
+        }
+    }
+}
