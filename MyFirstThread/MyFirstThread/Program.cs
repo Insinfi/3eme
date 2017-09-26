50ebe321d1de9796f0e3010eefdcdbac0cc2d18e
Insinfi <insinfigame@gmail.com>
2017-09-26 12:36:35 +0200

    01

---
+++ MyFirstThread/MyFirstThread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MyFirstThread
+{
+    class Program
+    {
+        static bool sortie = false;
+        static void Main(string[] args)
+        {
+            Thread test = new Thread(new ThreadStart(sayHi));
+            test.Start();
+            Console.ReadKey();
+            sortie = true;
+            Console.ReadKey();
+
+
+        }
+        static void sayHi()
+        {
+            while (sortie==false)
+            {
+                Console.WriteLine("Hi");
+
+            }
+        }
+    }
+}
