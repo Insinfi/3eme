f737f7dd95b06f87041a095ed4313a2da49440ef
Allan KNOCKAERT <zedsciences12@gmail.com>
2017-11-16 10:56:45 +0100

    Photo

---
+++ Trombinoscope/Trombinoscope/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trombinoscope
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel mwvm { get; set; }
+
+        public MainWindow()
+        {
+            mwvm = new MainWindowViewModel();
+            InitializeComponent();
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox tmp = (ListBox)sender;
+            GetAllUsersResult user = (GetAllUsersResult)tmp.SelectedItem;
+            mwvm.UpdateCurrentUser(user.UserID);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mwvm.UpdateUser();
+        }
+    }
+}
---
+++ Trombinoscope/Trombinoscope/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trombinoscope
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public List<GetAllUsersResult> UsersList { get; set; }
+        public GetUserInfoResult CurrentUser { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowViewModel()
+        {
+            DataClasses1DataContext MyContext = new DataClasses1DataContext();
+            UsersList = MyContext.GetAllUsers().ToList();
+        }
+
+        protected void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public void UpdateListUsers()
+        {
+            DataClasses1DataContext MyContext = new DataClasses1DataContext();
+            UsersList = MyContext.GetAllUsers().ToList();
+            OnPropertyChanged("UsersList");
+        }
+
+        public void UpdateCurrentUser(Guid UserId)
+        {
+            DataClasses1DataContext MyContext = new DataClasses1DataContext();
+            CurrentUser = MyContext.GetUserInfo(UserId).FirstOrDefault<GetUserInfoResult>();
+            OnPropertyChanged("CurrentUser");
+        }
+
+        public void UpdateUser()
+        {
+            DataClasses1DataContext MyContext = new DataClasses1DataContext();
+            MyContext.UpdateUser(CurrentUser.UserID, CurrentUser.Nom, CurrentUser.Prenom, CurrentUser.mail, CurrentUser.Tel,CurrentUser.GSM);
+        }
+    }
+}
