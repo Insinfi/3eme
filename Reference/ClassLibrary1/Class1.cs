1d795a1a37abdbe7813a89ab65f952f09b8515b3
Allan KNOCKAERT <zedsciences12@gmail.com>
2017-12-18 16:06:59 +0100

    Service

---
+++ Reference/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Personne
+    {
+        private string Nom;
+        private string Prenom;
+
+        public Personne(string Nom, string Prenom)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}",this.Nom, this.Prenom);
+        }
+    }
+}
---
+++ ServiceWindows/ServiceWindows/Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ServiceWindows
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Thread MyThread;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            MyThread = new Thread(new ThreadStart(Traitement));
+            MyThread.Start();
+        }
+
+        private void Traitement()
+        {
+            try
+            {
+                while (true)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+            MyThread.Abort();
+        }
+    }
+}
